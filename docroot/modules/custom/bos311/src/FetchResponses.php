832d33597324962e823556b5a81a825f71cf1b06
Adam Balsam <balsama@users.noreply.github.com>
2020-07-08 19:08:10 -0400

    Check for and update existing open reports (#2)
    On every cron run, load a random set of open reports and check to see if they are now closed.
    
    **Implementation:**
    1. Loads 250 open reports from the last two weeks
    1. Loads an additional 250 open reports from the last six months
    1. Checks to see if the status of each one has been changed from open to closed by pulling the report from the API
    1. If so, updates the status notes, status, and updated datetime fields and resaves the node.
    
    **In addition**
    * Request detail pages now conditionally show closed date and closed status notes for closed reports
    * List pages show an "open" or "closed" badge on cards
    * You can now filter on status when searching

--- docroot/modules/custom/bos311/src/FetchResponses.php
+++ docroot/modules/custom/bos311/src/FetchResponses.php
@@ -2,16 +2,22 @@
 
 namespace Drupal\bos311;
 
-use phpDocumentor\Reflection\Types\Integer;
+use Drupal\node\Entity\Node;
 
 class FetchResponses
 {
 
-    private array $services;
-    private $numberOfRecordsToGetPerRun = 500;
-    private $recordsSaved = 0;
-    private $recordsFailedToSave = 0;
-    private $apiRequestsMade = 0;
+    private $timestart;
+
+    private int $numberOfRecordsToGetPerRun = 500;
+    private int $numberOfExistingOpenRecordsToUpdate = 250;
+
+    private int $recordsSaved = 0;
+    private int $recordsFailedToSave = 0;
+    private int $apiRequestsMade = 0;
+    private int $openRecentRecordsClosed = 0;
+    private int $openOlderRecordsClosed = 0;
+
     private $serviceRequestId;
     private $rawRecord;
 
@@ -19,10 +25,13 @@ class FetchResponses
     private int $highestRemoteServiceRequestId;
     private int $lowestLocalServiceRequestId;
     private int $lowestRemoteServiceRequestId = 101000000000;
+    private array $openRecordsRecentNids;
+    private array $openRecordsOlderNids;
 
 
     public function __construct()
     {
+        $this->timestart = time();
         $this->findHighestRemoteServiceRequestId();
         $this->findHighestLocalServiceRequestId();
         $this->findLowestLocalServiceRequestId();
@@ -30,6 +39,7 @@ class FetchResponses
 
     public function doFetchRecords()
     {
+        $this->doUpdateExistingOpenRecords();
         $this->doFetchRecordsLi();
         $this->doFetchRecordsFi();
         $this->recordStatistics();
@@ -79,8 +89,50 @@ class FetchResponses
         $this->serviceRequestId = $this->serviceRequestId - 1;
         $this->doFetchRecordsFi();
     }
+    
+    private function doUpdateExistingOpenRecords() {
+        $this->openRecordsRecentNids = $this->findOpenRecords(14);
+        $this->updateOpenRecords($this->openRecordsRecentNids, 'Recent');
 
-    protected function fetchRawRecord() {
+        $this->openRecordsOlderNids = $this->findOpenRecords(180);
+        $this->updateOpenRecords($this->openRecordsOlderNids, 'Older');
+    }
+
+    private function updateOpenRecords($openNids, $name = 'Recent') {
+        foreach ($openNids as $openRecordNid) {
+            $typeString = 'open' . $name . 'RecordsClosed';
+            $node = Node::load($openRecordNid);
+            $serviceRequestId = $node->get('field_service_request_id')->value;
+            $response = Response::fetch(
+                "https://mayors24.cityofboston.gov/open311/v2/requests.json?service_request_id=$serviceRequestId"
+            );
+            $rawRecord = reset($response);
+
+            if ($rawRecord->status != 'closed') {
+                // This report is still open. Move on.
+                continue;
+            }
+            else {
+                $status_notes = (property_exists($rawRecord, 'status_notes')) ? $rawRecord->status_notes : 'No closing notes provided';
+                $updated_datetime = (property_exists($rawRecord, 'updated_datetime')) ? $rawRecord->updated_datetime : $rawRecord->requested_datetime;
+                $updateRecord = new UpdateRecord(
+                    $node,
+                    $status_notes,
+                    $updated_datetime,
+                    'closed'
+                );
+                $updateRecord->updateReportData();
+                if ($updatedRecord = $updateRecord->saveUpdatedExistingReport()) {
+                    $message = 'Updated: node/' . $updatedRecord->id() . ' <em>' . $updatedRecord->getTitle() . '</em> is now closed.';
+                    \Drupal::logger('Boston 311 Reports Content Updates')->notice($message);
+                    $updatedRecord->id();
+                    $this->$typeString++;
+                }
+            }
+        }
+    }
+
+    private function fetchRawRecord() {
         $rawRecord = Response::fetch("https://mayors24.cityofboston.gov/open311/v2/requests.json?service_request_id=$this->serviceRequestId");
         $this->apiRequestsMade++;
         $this->rawRecord = reset($rawRecord);
@@ -118,7 +170,7 @@ class FetchResponses
             }
         }
 
-        $highestRemoteServiceRequestId = $recordToUse->service_request_id;
+        $highestRemoteServiceRequestId = substr($recordToUse->service_request_id, -12);
 
         $this->highestRemoteServiceRequestId = $highestRemoteServiceRequestId;
     }
@@ -163,6 +215,27 @@ class FetchResponses
         $this->lowestLocalServiceRequestId = $lowestLocalServiceRequestId;
     }
 
+    /**
+     * @param int $daysOld
+     *   The number of days old reports can be.
+     *
+     * @return array
+     *   An array of node ids.
+     */
+    private function findOpenRecords(int $daysOld) {
+        $query = \Drupal::database()->select('node_field_data', 'n');
+        $query->join('node__field_status', 'nfs', 'n.nid = nfs.entity_id');
+        $query->join('node__field_requested_timestamp', 'nfr', 'n.nid = nfr.entity_id');
+        $query->fields('n', ['nid']);
+        $query->condition('type', 'report');
+        $query->condition('nfs.field_status_value', 'open');
+        $query->condition('nfr.field_requested_timestamp_value', time() - ($daysOld * 24 * 60 * 60), '>');
+        $query->range(0, $this->numberOfExistingOpenRecordsToUpdate);
+        $query->orderRandom();
+
+        return $query->execute()->fetchCol();
+    }
+
     private function processRecord() {
         $this->fetchRawRecord();
         if ($this->isValidRawRecord()) {
@@ -175,8 +248,11 @@ class FetchResponses
             'API calls:' => $this->apiRequestsMade,
             'Records saved:' => $this->recordsSaved,
             'Records failed' => $this->recordsFailedToSave,
+            'Recent records now closed' => $this->openRecentRecordsClosed,
+            'Older records now closed' => $this->openOlderRecordsClosed,
             'Start LI:' => $this->highestLocalServiceRequestId,
             'Start FI:' => $this->lowestLocalServiceRequestId,
+            'Execution time in seconds' => (time() - $this->timestart),
         ];
 
         $message = '';
@@ -187,6 +263,11 @@ class FetchResponses
         $message = "<ul>$message</ul>";
 
         \Drupal::logger('Boston 311 Reports')->notice($message);
+    }
+
+    private function ageOfOldestReportToUpdate() {
+        // Two weeks.
+        return time() - (14 * 24 * 60 * 60);
     }
 
 }
