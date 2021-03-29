8913baa86bfe1849ce09206bb88b94ea927abd9a
Adam Balsam <balsam.adam@gmail.com>
2021-03-28 20:15:10 -0400

    Add some old record back in now that its cought up

--- docroot/modules/custom/bos311/src/FetchResponses.php
+++ docroot/modules/custom/bos311/src/FetchResponses.php
@@ -9,8 +9,8 @@ class FetchResponses
 
     private $timestart;
 
-    private int $numberOfRecordsToGetPerRun = 59;
-    private int $numberOfExistingOpenRecordsToUpdate = 0;
+    private int $numberOfRecordsToGetPerRun = 40;
+    private int $numberOfExistingOpenRecordsToUpdate = 40;
 
     private int $recordsSaved = 0;
     private int $recordsFailedToSave = 0;
